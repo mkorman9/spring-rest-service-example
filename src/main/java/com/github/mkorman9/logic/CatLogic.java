59c8e8009c84a467717a623c2400311d7b7ad09e
mkorman9 <m.korman94@gmail.com>
2016-06-24 22:59:10 +0200

    Logging

--- src/main/java/com/github/mkorman9/logic/CatLogic.java
+++ src/main/java/com/github/mkorman9/logic/CatLogic.java
@@ -3,6 +3,8 @@ package com.github.mkorman9.logic;
 import com.github.mkorman9.dao.CatRepository;
 import com.github.mkorman9.model.Cat;
 import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +13,8 @@ import java.util.List;
 
 @Service
 public class CatLogic {
+    private static Logger LOGGER = LoggerFactory.getLogger(CatLogic.class);
+
     @Autowired
     private CatRepository catRepository;
     @Autowired
@@ -24,16 +28,19 @@ public class CatLogic {
     @Transactional
     public void addNewCat(CatData catData) {
         catRepository.save(catFactory.createEntity(catData));
+        LOGGER.info("Added new cat " + catData.toString());
     }
 
     @Transactional
     public void removeCat(Long id) {
         catRepository.delete(id);
+        LOGGER.info("Removed cat with id " + id);
     }
 
     @Transactional
     public void updateCat(Long id, CatData catData) {
         Cat entity = catRepository.findOne(id);
         catFactory.editEntity(entity, catData);
+        LOGGER.info("Updating cat with id " + id + " to " + entity.toString());
     }
 }
