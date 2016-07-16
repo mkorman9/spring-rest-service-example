b24b996c75d39fdb781652e0f08d5296765c0272
mkorman9 <m.korman94@gmail.com>
2016-07-16 18:22:55 +0200

    Extracted hibernate properties to separated file

--- src/main/java/com/github/mkorman9/JpaConfiguration.java
+++ src/main/java/com/github/mkorman9/JpaConfiguration.java
@@ -11,6 +11,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 @Configuration
@@ -28,34 +30,28 @@ public class JpaConfiguration {
     }
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource source, String cacheAddress) {
+    public Properties hibernateProperties(String cacheServerAddress) {
+        Properties properties = new Properties();
+        properties.setProperty("h4m.adapter.spymemcached.hosts", cacheServerAddress);
+
+        try {
+            InputStream resourceAsStream = getClass().getResourceAsStream("/META-INF/hibernate.properties");
+            properties.load(resourceAsStream);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot load Hibernate properties file", e);
+        }
+
+        return properties;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource source, Properties hibernateProperties) throws IOException {
         HibernateJpaVendorAdapter jpaVendorAdapter  = new HibernateJpaVendorAdapter();
         LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
         factoryBean.setDataSource(source);
         factoryBean.setJpaVendorAdapter(jpaVendorAdapter);
         factoryBean.setPackagesToScan("com.github.mkorman9.model");
-        factoryBean.setJpaProperties(new Properties() {
-            {
-                setProperty("hibernate.hbm2ddl.auto", "validate");
-                setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-
-                // Cache config
-                setProperty("hibernate.cache.use_second_level_cache", "true");
-                setProperty("hibernate.cache.use_query_cache", "true");
-                setProperty("hibernate.cache.default_cache_concurrency_strategy", "NONSTRICT_READ_WRITE");
-                setProperty("hibernate.cache.region.factory_class", "kr.pe.kwonnam.hibernate4memcached.Hibernate4MemcachedRegionFactory");
-                setProperty("hibernate.cache.region_prefix", "springtest");
-                setProperty("hibernate.cache.provider_configuration_file_resource_path", "META-INF/h4m-properties.xml");
-                setProperty("hibernate.cache.use_structured_entries", "false");
-                setProperty("h4m.adapter.class", "kr.pe.kwonnam.hibernate4memcached.spymemcached.SpyMemcachedAdapter");
-                setProperty("h4m.adapter.spymemcached.hosts", cacheAddress);
-                setProperty("h4m.adapter.spymemcached.hashalgorithm", "KETAMA_HASH");
-                setProperty("h4m.adapter.spymemcached.operation.timeout.millis", "5000");
-                setProperty("h4m.adapter.spymemcached.transcoder", "kr.pe.kwonnam.hibernate4memcached.spymemcached.KryoTranscoder");
-                setProperty("h4m.adapter.spymemcached.cachekey.prefix", "h4m");
-                setProperty("h4m.adapter.spymemcached.kryotranscoder.compression.threashold.bytes", "20000");
-            }
-        });
+        factoryBean.setJpaProperties(hibernateProperties);
         return factoryBean;
     }
 
