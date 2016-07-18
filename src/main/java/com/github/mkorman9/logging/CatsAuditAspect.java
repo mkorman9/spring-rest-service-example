8bbfda330ba207bd8be70ad9638fd83bbfa10596
mkorman9 <m.korman94@gmail.com>
2016-07-19 00:43:36 +0200

    Logging aspect refactoring

--- src/main/java/com/github/mkorman9/logging/CatsAuditAspect.java
+++ src/main/java/com/github/mkorman9/logging/CatsAuditAspect.java
@@ -2,7 +2,6 @@ package com.github.mkorman9.logging;
 
 import com.github.mkorman9.logic.CatLogic;
 import com.github.mkorman9.logic.data.CatData;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
@@ -14,21 +13,18 @@ import org.springframework.stereotype.Component;
 public class CatsAuditAspect {
     private static Logger LOGGER = LoggerFactory.getLogger(CatLogic.class);
 
-    @After("execution(* com.github.mkorman9.logic.CatLogic.addNewCat(..))")
-    public void logCatAdded(JoinPoint joinPoint) {
-        CatData catData = (CatData) joinPoint.getArgs()[0];
+    @After("execution(* com.github.mkorman9.logic.CatLogic.addNewCat(..)) && args(catData,..)")
+    public void logCatAdded(CatData catData) {
         LOGGER.info("Added new cat " + catData.getName());
     }
 
-    @After("execution(* com.github.mkorman9.logic.CatLogic.updateCat(..))")
-    public void logCatUpdated(JoinPoint joinPoint) {
-        Long id = (Long) joinPoint.getArgs()[0];
+    @After("execution(* com.github.mkorman9.logic.CatLogic.updateCat(..)) && args(id,..)")
+    public void logCatUpdated(Long id) {
         LOGGER.info("Updated cat with id " + id);
     }
 
-    @After("execution(* com.github.mkorman9.logic.CatLogic.removeCat(..))")
-    public void logCatRemoved(JoinPoint joinPoint) {
-        Long id = (Long) joinPoint.getArgs()[0];
+    @After("execution(* com.github.mkorman9.logic.CatLogic.removeCat(..)) && args(id,..)")
+    public void logCatRemoved(Long id) {
         LOGGER.info("Removed cat with id " + id);
     }
 }
