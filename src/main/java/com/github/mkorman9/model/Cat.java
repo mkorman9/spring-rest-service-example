002203be24983a87f1eaf06086f12f80a0deeb29
mkorman9 <m.korman94@gmail.com>
2016-08-07 21:22:26 +0200

    Imports fix

--- src/main/java/com/github/mkorman9/model/Cat.java
+++ src/main/java/com/github/mkorman9/model/Cat.java
@@ -7,7 +7,6 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
