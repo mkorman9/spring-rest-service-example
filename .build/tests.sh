6eaf400acfc9b2a6194d1ebd4b6c23c358b0dd35
mkorman9 <m.korman94@gmail.com>
2016-08-28 21:51:27 +0200

    Added temp smoke test

--- .build/tests.sh
+++ .build/tests.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-# TODO: Run tests here
+# Simple smoke test. Will be replaced with full test suite
+STATUS=$(curl -s "http://127.0.0.1:$APPLICATION_PORT/all" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["status"]')
+if [ "$STATUS" != 'ok' ]; then
+    echo $STATUS
+    exit 1
+fi
 
 exit 0
