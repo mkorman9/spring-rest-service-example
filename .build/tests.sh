fc7a2343130fe954745d74adc72ea43a7cfbc3ed
mkorman9 <m.korman94@gmail.com>
2016-08-28 22:14:22 +0200

    CI test

--- .build/tests.sh
+++ .build/tests.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 # Simple smoke test. Will be replaced with full test suite
-echo $(curl "http://127.0.0.1:$APPLICATION_PORT/all")
+timeout_counter="0"
+while [ $(curl -s "http://127.0.0.1:$APPLICATION_PORT/all") == '' ]; do
+    if [ $timeout_counter == "5" ]; then
+        break
+    fi
+
+    sleep 2
+    timeout_counter=$[$timeout_counter+1]
+done
+
 STATUS=$(curl -s "http://127.0.0.1:$APPLICATION_PORT/all" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["status"]')
 if [ "$STATUS" != 'ok' ]; then
     echo $STATUS
