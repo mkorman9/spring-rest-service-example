5270ee81e5492183cbcfbd420478745a6337cdcb
mkorman9 <m.korman94@gmail.com>
2016-08-28 22:01:11 +0200

    CI test

--- .build/tests.sh
+++ .build/tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Simple smoke test. Will be replaced with full test suite
-echo $(curl -s "http://127.0.0.1:$APPLICATION_PORT/all")
+echo $(curl "http://127.0.0.1:$APPLICATION_PORT/all")
 STATUS=$(curl -s "http://127.0.0.1:$APPLICATION_PORT/all" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["status"]')
 if [ "$STATUS" != 'ok' ]; then
     echo $STATUS
