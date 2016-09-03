617498057907e980cc7d315820e08cdbfb80eea6
mkorman9 <m.korman94@gmail.com>
2016-09-03 21:09:48 +0200

    Fixing functional tests keywords library

--- automated-tests/keywords.py
+++ automated-tests/keywords.py
@@ -7,4 +7,4 @@ def cat_should_exist_on_list(cats_json, role_name, name, duels_won, group_name):
         if cat['roleName'] == role_name and cat['name'] == name and cat['duelsWon'] == duels_won and cat['group']['name'] == group_name:
             exists = True
      if exists == False:
-        raise AssertionError('Specified cat (%s, %s, %d, %s) does not exists on list %s' % (role_name, name, duels_won, group_name, cats_json))
+        raise AssertionError('Specified cat (%s, %s, %s, %s) does not exists on list %s' % (role_name, name, duels_won, group_name, cats_json))
