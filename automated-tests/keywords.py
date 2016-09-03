902592040c9e98f1d86f55dfd38ac3608ca1694e
mkorman9 <m.korman94@gmail.com>
2016-09-03 21:18:52 +0200

    Fixing functional tests keywords library

--- automated-tests/keywords.py
+++ automated-tests/keywords.py
@@ -1,10 +1,13 @@
 import json
 
+def _compre_str(val1, val2):
+    return str(val1) == str(val2)
+
 def cat_should_exist_on_list(cats_json, role_name, name, duels_won, group_name):
      cats = json.loads(cats_json)
      exists = False
      for cat in cats:
-        if cat['roleName'] == role_name and cat['name'] == name and cat['duelsWon'] == duels_won and cat['group']['name'] == group_name:
+        if _compre_str(cat['roleName'], role_name) and _compre_str(cat['name'], name) and _compre_str(cat['duelsWon'], duels_won) and _compre_str(cat['group']['name'], group_name):
             exists = True
      if exists == False:
         raise AssertionError('Specified cat (%s, %s, %s, %s) does not exists on list %s' % (role_name, name, duels_won, group_name, cats_json))
