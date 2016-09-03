df1746ddf86448569fa22fee31bac7df78c7690c
mkorman9 <m.korman94@gmail.com>
2016-09-03 23:01:39 +0200

    Added test for single cat retrieve in functional tests, fix

--- automated-tests/keywords.py
+++ automated-tests/keywords.py
@@ -12,7 +12,7 @@ def _compare_cat_properties(cat, role_name, name, duels_won, group_name):
 def cat_should_be_equal_to(cat_json, role_name, name, duels_won, group_name):
     cat = json.loads(cat_json)
     if not _compare_cat_properties(cat, role_name, name, duels_won, group_name):
-        raise AssertionError('Cat %s is not equal to (%s, %s, %s, %s)' % (cat_json, role_name, name, duels_won, group_name)
+        raise AssertionError('Cat %s is not equal to (%s, %s, %s, %s)' % (cat_json, role_name, name, duels_won, group_name))
 
 
 def find_id_by_name(entity_json, name):
