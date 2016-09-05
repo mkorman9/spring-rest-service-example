ebb851e645671165629aefbf309e2ad4bdb7b7d2
mkorman9 <m.korman94@gmail.com>
2016-09-05 20:57:00 +0200

    Even more lombok

--- src/main/java/com/github/mkorman9/logging/CatsAuditAspect.java
+++ src/main/java/com/github/mkorman9/logging/CatsAuditAspect.java
@@ -2,6 +2,7 @@ package com.github.mkorman9.logging;
 
 import com.github.mkorman9.logic.CatLogic;
 import com.github.mkorman9.logic.dto.CatDto;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
@@ -10,21 +11,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
+@Slf4j
 public class CatsAuditAspect {
-    private static Logger LOGGER = LoggerFactory.getLogger(CatLogic.class);
-
     @After("execution(* com.github.mkorman9.logic.CatLogic.addNewCat(..)) && args(catDto,..)")
     public void logCatAdded(CatDto catDto) {
-        LOGGER.info("Added new cat " + catDto.getName());
+        log.info("Added new cat " + catDto.getName());
     }
 
     @After("execution(* com.github.mkorman9.logic.CatLogic.updateCat(..)) && args(id,..)")
     public void logCatUpdated(Long id) {
-        LOGGER.info("Updated cat with id " + id);
+        log.info("Updated cat with id " + id);
     }
 
     @After("execution(* com.github.mkorman9.logic.CatLogic.removeCat(..)) && args(id,..)")
     public void logCatRemoved(Long id) {
-        LOGGER.info("Removed cat with id " + id);
+        log.info("Removed cat with id " + id);
     }
 }
--- src/main/java/com/github/mkorman9/web/controller/ControllersCommons.java
+++ src/main/java/com/github/mkorman9/web/controller/ControllersCommons.java
@@ -4,6 +4,7 @@ import com.github.mkorman9.web.form.response.ResponseError;
 import com.github.mkorman9.web.form.response.ResponseForm;
 import com.github.mkorman9.web.form.response.ResponseStatus;
 import com.google.common.collect.ImmutableList;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -16,12 +17,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice(basePackages = "com.github.mkorman9.web.controller")
+@Slf4j
 class ControllersCommons {
-    private static Logger LOGGER = LoggerFactory.getLogger(ControllersCommons.class);
-
     @ExceptionHandler(Exception.class)
     public ResponseEntity exceptionHandler(Exception exception) {
-        LOGGER.error("Error during request processing", exception);
+        log.error("Error during request processing", exception);
 
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
