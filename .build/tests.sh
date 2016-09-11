b50f37e627bc1872329f0ca001b90ced6d8caeb1
mkorman9 <m.korman94@gmail.com>
2016-09-11 16:38:15 +0200

    Test dependencies file refactoring

--- .build/tests.sh
+++ .build/tests.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Install test dependencies
-$PIP_BINARY install --user robotframework-httplibrary
+python -c "import HttpLibrary" &> /dev/null
+if [ $? != 0 ]; then
+    $PIP_BINARY install --user robotframework-httplibrary
+fi
 
 # Wait until application wakes up
 timeout_counter="0"
 while [ "$(curl -s "http://127.0.0.1:$APPLICATION_PORT/")" == '' ]; do
     if [[ $timeout_counter == "10" ]]; then
-        echo "Application wake up phase timeout out"
+        echo "Application wake up phase timed out"
         exit 1
     fi
 
