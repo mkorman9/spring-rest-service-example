b46a058754fbf4c293a3d7d1bd7d8317c0958c85
mkorman9 <m.korman94@gmail.com>
2016-10-11 18:16:55 +0200

    Support for Eureka

--- src/main/java/com/github/mkorman9/SpringBootApp.java
+++ src/main/java/com/github/mkorman9/SpringBootApp.java
@@ -2,10 +2,12 @@ package com.github.mkorman9;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableEurekaClient
 public class SpringBootApp {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApp.class, args);
