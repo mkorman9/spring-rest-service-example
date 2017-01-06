d160ec294ec82adc60522f15877df2821f25acfa
mkorman9 <m.korman94@gmail.com>
2017-01-06 12:07:34 +0100

    introduce tuples

--- spring-rest-service-example-app/src/main/java/com/github/mkorman9/MessagingConfiguration.java
+++ spring-rest-service-example-app/src/main/java/com/github/mkorman9/MessagingConfiguration.java
@@ -1,5 +1,7 @@
 package com.github.mkorman9;
 
+import javaslang.Tuple;
+import javaslang.Tuple2;
 import lombok.val;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
@@ -27,7 +29,7 @@ public class MessagingConfiguration {
     @Bean
     public ConnectionFactory connectionFactory() {
         val addressParts = parseBrokerAddress(brokerAddress);
-        val connectionFactory = new CachingConnectionFactory(addressParts[0], Integer.valueOf(addressParts[1]));
+        val connectionFactory = new CachingConnectionFactory(addressParts._1, Integer.valueOf(addressParts._2));
         connectionFactory.setUsername(brokerUsername);
         connectionFactory.setPassword(brokerPassword);
         return connectionFactory;
@@ -46,11 +48,11 @@ public class MessagingConfiguration {
         return new Queue(QUEUE_NAME, true);
     }
 
-    private String[] parseBrokerAddress(String brokerAddress) {
+    private Tuple2<String, String> parseBrokerAddress(String brokerAddress) {
         val parts = brokerAddress.split(":");
         return Match(parts.length).of(
-                Case($(1), new String[] { parts[0], "5672" }),
-                Case($(), parts)
+                Case($(1), Tuple.of(parts[0], "5672")),
+                Case($(), Tuple.of(parts[0], parts[1]))
         );
     }
 }
