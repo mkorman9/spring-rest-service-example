98ac8f994ed11ad2e961a23f597e7259087978a9
Michał Korman <mkorman9@users.noreply.github.com>
2017-01-10 13:47:16 +0100

    Update ControllersCommons.java

--- spring-rest-service-example-app/src/main/java/com/github/mkorman9/web/controller/ControllersCommons.java
+++ spring-rest-service-example-app/src/main/java/com/github/mkorman9/web/controller/ControllersCommons.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.util.stream.Collectors;
 
@@ -53,6 +54,7 @@ class ControllersCommons {
     private HttpStatus resolveResponseStatus(Exception exception) {
         return Match(exception).of(
                 Case(instanceOf(InvalidInputDataException.class), HttpStatus.BAD_REQUEST),
+                Case(instanceOf(MethodArgumentNotValidException.class), HttpStatus.BAD_REQUEST),
                 Case($(), HttpStatus.INTERNAL_SERVER_ERROR)
         );
     }
@@ -60,6 +62,7 @@ class ControllersCommons {
     private String resolveResponseText(Exception exception) {
         return Match(exception).of(
                 Case(instanceOf(InvalidInputDataException.class), exception.getMessage()),
+                Case(instanceOf(MethodArgumentNotValidException.class), exception.getMessage()),
                 Case($(), INTERNAL_ERROR_RESPONSE_TEXT)
         );
     }
