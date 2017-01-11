45c7fc650614fe8714f0af6a8acb911d6dbb6a1b
Michal Korman <michal.korman@nokia.com>
2017-01-11 17:10:29 +0100

    tests scripts refactor

--- .build/tests.sh
+++ .build/tests.sh
@@ -6,45 +6,56 @@ function print_logs() {
     echo '================================================='
 }
 
-# Install test dependencies
-python -c "import pip" &> /dev/null
-if [ $? != 0 ]; then
-    curl -o /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py
-    python /tmp/get-pip.py --user
-fi
+function install_pip() {
+    python -c "import pip" &> /dev/null
+    if [ $? != 0 ]; then
+        curl -o /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py
+        python /tmp/get-pip.py --user
+    fi
+}
 
-python -c "import behave" &> /dev/null
-if [ $? != 0 ]; then
-    python -m pip install --user behave
-fi
-
-python -c "import requests" &> /dev/null
-if [ $? != 0 ]; then
-    python -m pip install --user requests
-fi
-
-# Wait until application wakes up
-timeout_counter="0"
-while true; do
-    status_code=$(curl -s -o /dev/null -w "%{http_code}" "http://127.0.0.1:$APPLICATION_PORT/")
-    echo "Trying http://127.0.0.1:$APPLICATION_PORT/... ${status_code}"
-
-    if [ "$status_code" == "200" ] || [ "$status_code" == "404" ]; then
-        break
+function install_test_dependencies() {
+    python -c "import behave" &> /dev/null
+    if [ $? != 0 ]; then
+        python -m pip install --user behave
     fi
 
-    if [[ $timeout_counter == "20" ]]; then
-        echo "Application wake up phase timed out"
-        print_logs
-        exit 1
+    python -c "import requests" &> /dev/null
+    if [ $? != 0 ]; then
+        python -m pip install --user requests
     fi
+}
 
-    sleep 2
-    timeout_counter=$[$timeout_counter+1]
-done
+function wait_for_application_to_wake_up() {
+    timeout_counter="0"
+    while true; do
+        status_code=$(curl -s -o /dev/null -w "%{http_code}" "http://127.0.0.1:$APPLICATION_PORT/")
+        echo "Trying http://127.0.0.1:$APPLICATION_PORT/... ${status_code}"
 
-python -m behave spring-rest-service-example-tests/src/behave
+        if [ "$status_code" == "200" ] || [ "$status_code" == "404" ]; then
+            break
+        fi
 
+        if [[ $timeout_counter == "20" ]]; then
+            echo "Application wake up phase timed out"
+            print_logs
+            exit 1
+        fi
+
+        sleep 2
+        timeout_counter=$[$timeout_counter+1]
+    done
+}
+
+function run_tests() {
+    python -m behave spring-rest-service-example-tests/src/behave
+}
+
+install_pip
+install_test_dependencies
+
+wait_for_application_to_wake_up
+run_tests
 print_logs
 
 if [ $? != 0 ]; then
