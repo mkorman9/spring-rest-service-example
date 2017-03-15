765fe70ed10c58ca1570d38a7f8e9523a5ec3b9d
Michal Korman <michal.korman@nokia.com>
2017-03-15 12:15:38 +0100

    update spring boot version #5

--- spring-rest-service-example-app/src/main/java/com/github/mkorman9/entity/Cat.java
+++ spring-rest-service-example-app/src/main/java/com/github/mkorman9/entity/Cat.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -24,7 +25,7 @@ import java.io.Serializable;
 @ToString
 public class Cat implements Serializable {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID", unique = true, nullable = false)
     private Long id;
 
--- spring-rest-service-example-app/src/main/java/com/github/mkorman9/entity/CatsGroup.java
+++ spring-rest-service-example-app/src/main/java/com/github/mkorman9/entity/CatsGroup.java
@@ -9,6 +9,7 @@ import lombok.ToString;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -21,7 +22,7 @@ import javax.persistence.Table;
 @ToString
 public class CatsGroup {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID", nullable = false, unique = true)
     private Long id;
 
