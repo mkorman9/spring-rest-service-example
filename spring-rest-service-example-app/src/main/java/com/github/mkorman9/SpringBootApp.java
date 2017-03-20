a39ab5d9baa557dc73f6fd3230f4db7529b92cd1
Michal Korman <michal.korman@nokia.com>
2017-03-20 15:53:35 +0100

    disable cglib in aspectes

--- spring-rest-service-example-app/src/main/java/com/github/mkorman9/SpringBootApp.java
+++ spring-rest-service-example-app/src/main/java/com/github/mkorman9/SpringBootApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableAspectJAutoProxy
 public class SpringBootApp {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApp.class, args);
