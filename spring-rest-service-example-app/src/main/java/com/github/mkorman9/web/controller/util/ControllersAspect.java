8b2974da0a2f26c7d2a46f19fd473af523427834
mkorman9 <m.korman94@gmail.com>
2017-04-04 20:13:15 +0200

    better Streams  examples

--- spring-rest-service-example-app/src/main/java/com/github/mkorman9/web/controller/util/ControllersAspect.java
+++ spring-rest-service-example-app/src/main/java/com/github/mkorman9/web/controller/util/ControllersAspect.java
@@ -1,9 +1,11 @@
 package com.github.mkorman9.web.controller.util;
 
+import com.codepoetics.protonpack.StreamUtils;
 import com.github.mkorman9.logic.exception.InvalidInputDataException;
 import com.github.mkorman9.web.form.response.ResponseError;
 import com.github.mkorman9.web.form.response.ResponseForm;
 import com.github.mkorman9.web.form.response.ResponseStatus;
+import javaslang.Tuple;
 import javaslang.control.Try;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -33,10 +36,12 @@ public class ControllersAspect {
     }
 
     @Pointcut("execution(public java.lang.Object *(..))")
-    public void publicMethodReturningObject() { }
+    public void publicMethodReturningObject() {
+    }
 
     @Pointcut("@within(org.springframework.web.bind.annotation.RequestMapping)")
-    public void requestMappingAnnotated() { }
+    public void requestMappingAnnotated() {
+    }
 
     @Around("publicMethodReturningObject() && requestMappingAnnotated()")
     public ResponseForm executeControllerCode(ProceedingJoinPoint joinPoint) throws Throwable {
@@ -45,9 +50,9 @@ public class ControllersAspect {
         return Try.of(joinPoint::proceed)
                 .map(returnObject ->
                         ResponseForm.builder()
-                        .status(ResponseStatus.OK)
-                        .data(returnObject)
-                        .build()
+                                .status(ResponseStatus.OK)
+                                .data(returnObject)
+                                .build()
                 )
                 .getOrElseThrow(
                         exc -> exc
@@ -73,23 +78,25 @@ public class ControllersAspect {
     private List<ResponseError> createErrorsList(Set<ConstraintViolation<Object>> violations) {
         return violations.stream()
                 .map(error -> ResponseError.builder()
-                                .message(error.getMessage())
-                                .field(error.getPropertyPath().toString())
-                                .build()
+                        .message(error.getMessage())
+                        .field(error.getPropertyPath().toString())
+                        .build()
                 )
                 .collect(Collectors.toList());
     }
 
     private Optional<Object> findRequestBodyToValidate(Object[] arguments, Annotation[][] annotationsOverArguments) {
-        for (int i = 0; i < arguments.length; i++) {
-            for (Annotation annotation : annotationsOverArguments[i]) {
-                if (annotation instanceof RequestBody) {
-                    return Optional.of(arguments[i]);
-                }
-            }
-        }
-
-        return Optional.empty();
+        return StreamUtils.zip(
+                Arrays.stream(arguments),
+                Arrays.stream(annotationsOverArguments),
+                (argument, annotations) -> Tuple.of(
+                        argument,
+                        Arrays.stream(annotations)
+                                .anyMatch(annotation -> annotation instanceof RequestBody)
+                ))
+                .filter(argumentAndAnnotationExistenceTuple -> argumentAndAnnotationExistenceTuple._2 == Boolean.TRUE)
+                .map(argumentAndAnnotationExistenceTuple -> argumentAndAnnotationExistenceTuple._1)
+                .findFirst();
     }
 
     private Annotation[][] getAnnotationsFromMethodParameters(MethodSignature method) throws NoSuchMethodException {
